a2d3f5616cc3eeb58ed6fdca0cf80cb0d5e4c3e6
DongWook Lee <vztpv@naver.com>
2017-01-04 16:11:07 +0900

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -23,11 +23,18 @@ using namespace std;
 
 #include <conio.h>
 
+
+#include <tbb/blocked_range.h>
+#include <tbb/parallel_for.h>
+#include <tbb/parallel_reduce.h>
+
 #include <wiz/load_data.h>
 #include <wiz/stacks.h>
 #include <wiz/cpp_string.h>
 
 #include <Windows.h>
+
+//
 
 
 inline int GETCH() /// To Do - Replace..
